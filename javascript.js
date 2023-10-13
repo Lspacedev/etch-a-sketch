7b04062adade230c0784f9f92ff962569399c959
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2023-10-13 11:26:25 +0200

    Replaced prompt with input slider, grid size not set dynamically

--- javascript.js
+++ javascript.js
@@ -1,21 +1,37 @@
 const container = document.querySelector('.container');
+const gridNum = document.querySelector('#gridNum');
+
+
+const btnSmall = document.querySelector('#gridAdd');
+
+
+gridNum.textContent = digi;
+
+
+function clearGrid(){
+  container.querySelectorAll('*').forEach(n => n.remove());
+}
+
+function getNum(e){
+  x = parseInt(e);
+  
+  clearGrid();
 //create row divs
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < x; i++){
+
     const divRow= document.createElement('div');
     divRow.classList.add('row');
     container.appendChild(divRow);
 
     //create column divs
 
-    for(let k = 0; k < 16; k++){
+    for(let k = 0; k < x; k++){
         const divCol= document.createElement('div');
         divCol.classList.add('col');
-        divRow.appendChild(divCol)
+        divRow.appendChild(divCol);
     }
 
 }
-console.log(container);
 
-
-
+}
